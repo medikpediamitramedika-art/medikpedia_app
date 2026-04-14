da5c13e1cc65f9c464a4675d7ef2e99c0428dd7d
and-221 <verleamapa@gmail.com>
2026-04-14 13:09:46 +0700

    as

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -6,6 +6,9 @@
     <title>@yield('title', 'Medikpedia - Apotik Online')</title>
     <link rel="icon" type="image/png" href="{{ asset('logo1.png') }}">
     
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     
@@ -1137,5 +1140,8 @@
             }
         });
     </script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
